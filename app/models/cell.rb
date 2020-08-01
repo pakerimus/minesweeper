468441a7b6942afbf0a57189f044a1934e396798
Paco Moreno <pakerimus@gmail.com>
2020-08-01 20:28:16 -0300

    cell scopes

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -8,4 +8,12 @@ class Cell < ApplicationRecord
   }
 
   validates :row, :column, presence: true, numericality: { only_integer: true }
+
+  scope :bombs, -> { where(bomb: true) }
+  scope :normal, -> { where(bomb: false) }
+
+  scope :cleared, -> { where(cleared: true) }
+  scope :not_cleared, -> { where(cleared: false) }
+
+  scope :by_position, ->(row, column) { where(row: row, column: column) }
 end
