002ae532318dc3413723d346fe5219acdf8e699a
Paco Moreno <pakerimus@gmail.com>
2020-08-03 01:40:25 -0300

    grid & style

---
+++ app/javascript/cell.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="game-cell" :class="cellColorClass">
+    <div v-if="showIcon">
+      <i :class="cellIconClass"></i>
+    </div>
+    <div v-if="!showIcon">
+      {{ cellTitle }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cell",
+  props: {
+    cell: {
+      type: Object,
+      required: true
+    }
+  },
+  data: function () {
+    return {
+      loading: false
+    }
+  },
+  computed: {
+    showIcon() {
+      if (!this.cell.cleared && !this.cell.bomb) {
+        return true;
+      } else if (this.cell.cleared && this.cell.bomb) {
+        return true;
+      } else if (this.cell.mark !== "no_mark") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    cellColorClass() {
+      if (this.cell.cleared && this.cell.bomb) {
+        return "game-bomb";
+      } else if (this.cell.cleared && !this.cell.bomb) {
+        return "game-cleared";
+      } else if (this.cell.mark == "question") {
+        return "game-mark-with-question";
+      } else if (this.cell.mark == "with_bomb") {
+        return "game-mark-with-bomb";
+      } else {
+        return "";
+      }
+    },
+    cellIconClass() {
+      if (this.cell.cleared && this.cell.bomb) {
+        return "el-icon-error";
+      } else if (this.cell.mark == "question") {
+        return "el-icon-question";
+      } else if (this.cell.mark == "with_bomb") {
+        return "el-icon-s-flag";
+      } else {
+        return "";
+      }
+    },
+    cellTitle() {
+      if (this.cell.adjacent_bombs == 0) return "";
+      return this.cell.adjacent_bombs;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .game {
+    &-cell {
+      align-items: center;
+      background: rgba(0, 0, 0, 0.1);
+      border: 1px white solid;
+      color: #2c3e50;
+      cursor: pointer;
+      display: flex;
+      font-size: 1.3em;
+      justify-content: center;
+      min-height: 35px;
+      min-width: 35px;
+    }
+    &-bomb {
+      background: #c0392b;
+    }
+    &-cleared {
+      background: rgba(0, 0, 0, 0.05);
+    }
+    &-mark-with-question {
+      background: #E6A23C;
+    }
+    &-mark-with-bomb {
+      background: #67C23A;
+    }
+  }
+</style>
--- app/javascript/game.vue
+++ app/javascript/game.vue
@@ -1,19 +1,48 @@
 <template>
   <div v-loading="loading">
     <div v-if="game.id" >
-      <el-page-header @back="goBack" title="Go back" :content="title" />
+      <el-page-header @back="goBack" title="Go back">
+        <template slot="content">
+          <span>Game #{{ game.id }}</span>
+          <el-divider direction="vertical"></el-divider>
+          <span>Rows: {{ game.height }}</span>
+          <el-divider direction="vertical"></el-divider>
+          <span>Columns: {{ game.width }}</span>
+          <el-divider direction="vertical"></el-divider>
+          <span>Bombs: {{ game.bombs }}</span>
+        </template>
+      </el-page-header>
+      <el-divider />
       <div class="page-content">
-        {{ game }}
+        <el-row :gutter="20">
+          <el-col :span="8"><span>Actions</span></el-col>
+          <el-col :span="8">{{ gameMessage }}</el-col>
+          <el-col :span="8"><span>Timer</span></el-col>
+        </el-row>
         <hr>
-        {{ cells }}
+        <div id="game-board">
+          <div class="game-grid" :style="getGridStyle()">
+            <cell
+              v-for="(cell, i) in cells"
+              :key="i"
+              :cell="cell"
+              @click.native="clearCell(cell)"
+              @click.right.native="cycleCellFlag(cell)"
+              @contextmenu.native.prevent
+            />
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Cell from "./cell";
+
 export default {
   name: "Game",
+  components: { Cell },
   props: {
     userId: {
       type: Number,
@@ -27,16 +56,27 @@ export default {
   data: function () {
     return {
       loading: false,
-      gameState: null,
+      gameMessage: null,
       game: {
-        id: null
+        id: null,
+        state: null,
+        width: null,
+        height: null,
+        bombs: null,
+        last_started_at: null,
+        total_time: null,
+        available_plays: null
       },
+      timer: 0,
       cells: []
     }
   },
   computed: {
     title() {
       return `Game #${this.game.id}`
+    },
+    gameUrl() {
+      return `/api/users/${this.userId}/games/${this.gameId}`;
     }
   },
   created() {
@@ -45,11 +85,13 @@ export default {
   methods: {
     getGame() {
       this.loading = true;
-      this.$http.get(`/api/users/${this.userId}/games/${this.gameId}`)
+      this.$http.get(this.gameUrl)
         .then(response => {
-          console.log("game response", response.body);
           this.game = response.body.game;
           this.cells = response.body.cells;
+          if (this.game.state == "pending") {
+            this.gameMessage = "Start the game by clicking in any cell"
+          }
         })
         .catch(error => {
           console.log("game error", error);
@@ -60,17 +102,13 @@ export default {
         });
     },
     goBack() {
-      if (this.gameState == "started") this.sendGameAction("pause");
+      if (this.game.state == "started") this.sendGameAction("pause");
       this.$emit('clearGame');
-    },
-    sendGameAction(action) {
-      console.log("sendGameAction", action);
     },
     refreshCells() {
       this.loading = true;
       this.$http.get(`/api/users/${this.userId}/games/${this.game.id}/cells`)
         .then(response => {
-          console.log("cells response", response.body);
           this.cells = response.body.cells;
         })
         .catch(error => {
@@ -80,7 +118,54 @@ export default {
         .finally(() => {
           this.loading = false;
         });
+    },
+    getGridStyle() {
+      return `grid-template-columns: repeat(${this.game.width}, 40px);`;
+    },
+    clearCell(cell) {
+      this.sendCellAction(cell, "clear");
+    },
+    cycleCellFlag(cell) {
+      this.sendCellAction(cell, "cycle_mark");
+    },
+    sendGameAction(action) {
+      let mustRefreshBoard = false;
+      const url = `${this.gameUrl}/execute`;
+      console.log("sendGameAction", url, action);
+      if (mustRefreshBoard) this.refreshCells();
+    },
+    sendCellAction(cell, action) {
+      let mustRefreshBoard = false;
+      const url = `${this.gameUrl}/cells/${cell.id}/execute`;
+      console.log("sendCellAction", url, action);
+      if (mustRefreshBoard) this.refreshCells();
     }
   }
 }
 </script>
+
+<style lang="scss">
+  .game {
+    &-grid {
+      user-select: none;
+      position: relative;
+      overflow: auto;
+      justify-content: center;
+      align-content: center;
+      display: grid;
+      grid-template-columns: repeat(9, 1fr);
+      grid-auto-rows: 1fr;
+      &:before {
+        content: '';
+        width: 0;
+        padding-bottom: 100%;
+        grid-row: 1 / 1;
+        grid-column: 1 / 1;
+      }
+      > *:first-child {
+        grid-row: 1 / 1;
+        grid-column: 1 / 1;
+      }
+    }
+  }
+</style>
